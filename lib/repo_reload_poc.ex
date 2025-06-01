f85bed3927b3f8621309417908476c88824d87b9
Nicholas Moen <arcanemachine@gmail.com>
2025-05-31 18:36:13 -0600

    Add custom reload code

--- lib/repo_reload_poc.ex
+++ lib/repo_reload_poc.ex
@@ -1,3 +1,37 @@
 defmodule RepoReloadPoc do
-  :ok
+  alias RepoReloadPoc.Repo
+
+  @doc "Similar to `Repo.reload/2`, but supports structs with multiple primary keys."
+  def reload(%_{} = struct, opts \\ []) do
+    case struct.__struct__.__schema__(:primary_key) do
+      [_pk] ->
+        Repo.reload(struct, opts)
+
+      _pks ->
+        get_by_clauses =
+          struct.__struct__.__schema__(:primary_key)
+          |> Keyword.new(fn primary_key_field ->
+            {primary_key_field, Map.fetch!(struct, primary_key_field)}
+          end)
+
+        Repo.get_by(struct.__struct__, get_by_clauses, opts)
+    end
+  end
+
+  @doc "Similar to `Repo.reload!/2`, but supports structs with multiple primary keys."
+  def reload!(%_{} = struct, opts \\ []) do
+    case struct.__struct__.__schema__(:primary_key) do
+      [_pk] ->
+        Repo.reload!(struct, opts)
+
+      _pks ->
+        get_by_clauses =
+          struct.__struct__.__schema__(:primary_key)
+          |> Keyword.new(fn primary_key_field ->
+            {primary_key_field, Map.fetch!(struct, primary_key_field)}
+          end)
+
+        Repo.get_by!(struct.__struct__, get_by_clauses, opts)
+    end
+  end
 end
